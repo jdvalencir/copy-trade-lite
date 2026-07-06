0ae8f1c1795550624d929f3fa445f308f8ea7245
Julian Valencia <julianv302@gmail.com>
2026-07-05 19:48:11 -0500

    fix: reorder import statement for Signal in SignalLeaderboard component

--- src/components/SignalLeaderboard.tsx
+++ src/components/SignalLeaderboard.tsx
@@ -1,5 +1,5 @@
-import { Signal } from "@/lib/signals";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Signal } from "@/lib/signals";
 
 type Row = {
   author: string;
